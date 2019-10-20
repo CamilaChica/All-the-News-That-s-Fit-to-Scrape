42a595449aae4eb633846cb4f761ca01479b1451
Camila Chica <50186503+CamilaChica@users.noreply.github.com>
2019-10-19 23:11:51 -0400

    scrape function added

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get("/note/:id", function(req, res){
 
 app.post("/note/:id", function(req, res){
     var tempNote = new notes(req.body);
-    notes.saved(function(err, doc){
+    notes.save(function(err, doc){
         if(err) throw err;
         articles.findByIdAndUpdate(req.params.id, {$set: {"note": doc._id}}, {new: true}, function(err, res1){
             if(err) throw err;
@@ -115,4 +115,38 @@ app.post("/note/:id", function(req, res){
             }
         });
     });
+});
+
+app.get("/scrape", function(req, res){
+    request("https://www.nytimes.com/section/world", function(error, response, html) {
+		var $ = cheerio.load(html);
+		var result = {};
+		$("div.story-body").each(function(i, element) {
+			var link = $(element).find("a").attr("href");
+			var title = $(element).find("h2.headline").text().trim();
+			var summary = $(element).find("p.summary").text().trim();
+			var img = $(element).parent().find("figure.media").find("img").attr("src");
+			result.link = link;
+			result.title = title;
+			if (summary) {
+				result.summary = summary;
+			};
+			if (img) {
+				result.img = img;
+			}
+			else {
+				result.img = $(element).find(".wide-thumb").find("img").attr("src");
+			};
+			var entry = new Article(result);
+			articles.find({title: result.title}, function(err, data) {
+				if (data.length === 0) {
+					entry.save(function(err, data) {
+						if (err) throw err;
+					});
+				}
+			});
+		});
+		console.log("Scrape finished.");
+		res.redirect("/");
+	});
 });
