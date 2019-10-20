7e22730da0a433a33e1e065e59b6bc243b7dd450
Camila Chica <50186503+CamilaChica@users.noreply.github.com>
2019-10-19 23:21:19 -0400

    event to show the notes created :P

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,16 @@
+function shownote(event) {
+	event.preventDefault();
+	var id = $(this).attr("value");
+	$("#addnote").fadeIn(300).css("display", "flex");
+	$("#add-note").attr("value", id);
+	$.get("/" + id, function(data) {
+		$("#article-title").text(data.title);
+		$.get("/note/" + id, function(data) {
+			if (data) {
+				$("#note-title").val(data.title);
+				$("#note-body").val(data.body);
+			}
+		});
+	});
+
+}
